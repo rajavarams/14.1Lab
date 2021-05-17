062c0e4d472313866c06c0ed7f6ea73018cf5100
Sravanthi Rajavaram <79155101+rajavarams@users.noreply.github.com>
2021-05-17 15:29:42 -0400

    first commit

---
+++ src/main/java/com/tts/Week14Day1Lab/Week14Day1LabApplication.java
@@ -0,0 +1,13 @@
+package com.tts.Week14Day1Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week14Day1LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week14Day1LabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/Week14Day1Lab/Week14Day1LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.Week14Day1Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week14Day1LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
